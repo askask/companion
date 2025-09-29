59c263733820d07d31f71532674ea44ade16b099
Andreas Kling <andreas@akling.org>
2025-09-29 20:00:00 +0200

    test

--- companion/lib/Surface/USB/MiraboxStreamDock.ts
+++ companion/lib/Surface/USB/MiraboxStreamDock.ts
@@ -1818,7 +1818,7 @@ class StreamDock extends EventEmitter {
 			this.model = StreamDock.models['HSV 293S-2']
 			this.packetSize = 512
 		} else if (this.info.productId === 0x3010) {
-			this.model = StreamDock.models['293V3']
+			this.model = StreamDock.models['HSV 293S-2']
 		} else {
 			// this.modelType = 'Unknown'
 			this.emit('remove')
